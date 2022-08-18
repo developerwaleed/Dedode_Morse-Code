3698cc9d12cb044231c59b7885e323fad51c7092
Waleed Amjad <waleedamjad99@hotmail.com>
2022-08-18 10:38:41 +0500

    Add condtions to decode message

--- morse_code.rb
+++ morse_code.rb
@@ -9,7 +9,19 @@ decoded_message = ""
       'Q' => '--.-', 'R' => '.-.', 'S' => '...', 'T' => '-',
       'U' => '..-', 'V' => '...-', 'W' => '.--', 'X' => '-..-',
       'Y' => '-.--', 'Z' => '--..'
-    }      
+    }
+
+    str = char.split(/ /)
+    str.each do |item|
+        if morse_dict.key(item)
+                    decoded_message = decoded_message + morse_dict.key(item)
+        elsif item == ""
+            decoded_message = decoded_message + " "
+           
+        else 
+            invalid = false;
+      end
+    end      
 end
 
 decode_char(" .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
